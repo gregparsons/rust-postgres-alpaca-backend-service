54bccce2be8b6734ff27103e1dd5cc7faa611853
gp <gp@willamette.local>
2023-07-25 23:08:14 -0700

    sqlx

--- push_backend.sh
+++ push_backend.sh
@@ -1,5 +1,11 @@
-#make sqlx; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205 "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make -B backend"
+make sqlx; 
+git add .;
+git commit -am "sqlx"; 
+source ~/.bashrc; git push origin dev; ssh swimr205 "cd dev/rust/_trade/trade;
+export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; 
+git pull origin dev;
+make -B backend"
 
 # swimr205remote
-make sqlx; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205remote "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make -B backend"
+#make sqlx; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205remote "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make -B backend"
 
