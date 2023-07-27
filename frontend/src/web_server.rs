1dc720f5026b95c29e5b649af559e97d67142add
gp <github@swimr.com>
2023-07-27 14:29:13 -0700

    TLS

--- frontend/src/web_server.rs
+++ frontend/src/web_server.rs
@@ -1,5 +1,7 @@
 //! web_server.rs
 
+use std::fs;
+use std::io::{BufReader};
 use crate::configuration::get_yaml_configuration;
 use actix_session::storage::CookieSessionStore;
 use actix_session::SessionMiddleware;
@@ -7,7 +9,6 @@ use actix_web::{web, App, HttpServer};
 use handlebars::Handlebars;
 use sqlx::PgPool;
 use common_lib::db::DbMsg;
-// use crate::signup::{_get_signup, _post_signup};
 use crate::account::get_account;
 use crate::activities::{get_activities, get_activity_for_symbol};
 use crate::dashboard::{get_dashboard, get_dashboard_with_symbol};
@@ -73,6 +74,24 @@ impl WebServer {
         let db_pool_data = web::Data::new(pool);
         let tx_db_data = web::Data::new(tx_db.clone());
 
+        // TLS
+        // https://github.com/rustls/rustls/blob/main/examples/src/bin/tlsserver-mio.rs
+        // let certs = load_certs("/Users/gp/trade/frontend/certificate/cert.pem");
+        // let privkey = load_private_key("/Users/gp/trade/frontend/certificate/key.pem");
+        let certs = load_certs("frontend/static/certificate/cert.pem");
+        let privkey = load_private_key("frontend/static/certificate/key.pem");
+        // let ocsp = load_ocsp(&args.flag_ocsp);
+
+        let config = rustls::ServerConfig::builder()
+            .with_cipher_suites(&rustls::ALL_CIPHER_SUITES.to_vec())
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(&rustls::ALL_VERSIONS.to_vec())
+            .expect("inconsistent cipher-suites/versions specified")
+            // .with_client_cert_verifier(NoClientAuth::)
+            // .with_single_cert_with_ocsp(certs, privkey, ocsp)
+            .with_no_client_auth()
+            .with_single_cert(certs, privkey)
+            .expect("bad certificates/private key");
 
         let server = HttpServer::new(move || {
             App::new()
@@ -113,10 +132,57 @@ impl WebServer {
                 .route("/order", web::get().to(get_order))
             // .route("/order/{symbol}", web::get().to(get_orders))
         })
-        .bind(("0.0.0.0", web_port))?
+        .bind_rustls(("0.0.0.0", web_port), config)?
         .workers(2)
         .run();
 
         server.await
     }
 }
+
+
+fn load_private_key(filename: &str) -> rustls::PrivateKey {
+    let keyfile = fs::File::open(filename).expect("cannot open private key file");
+    let mut reader = BufReader::new(keyfile);
+
+
+
+    loop {
+        match rustls_pemfile::read_one(&mut reader).expect("cannot parse private key .pem file") {
+            Some(rustls_pemfile::Item::RSAKey(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::PKCS8Key(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::ECKey(key)) => return rustls::PrivateKey(key),
+            None => break,
+            _ => {}
+        }
+    }
+
+    panic!(
+        "no keys found in {:?} (encrypted keys not supported)",
+        filename
+    );
+}
+
+
+fn load_certs(filename: &str) -> Vec<rustls::Certificate> {
+    let certfile = fs::File::open(filename).expect("cannot open certificate file");
+    let mut reader = BufReader::new(certfile);
+    rustls_pemfile::certs(&mut reader)
+        .unwrap()
+        .iter()
+        .map(|v| rustls::Certificate(v.clone()))
+        .collect()
+}
+
+// fn load_ocsp(filename: &Option<String>) -> Vec<u8> {
+//     let mut ret = Vec::new();
+//
+//     if let Some(name) = filename {
+//         fs::File::open(name)
+//             .expect("cannot open ocsp file")
+//             .read_to_end(&mut ret)
+//             .unwrap();
+//     }
+//
+//     ret
+// }
