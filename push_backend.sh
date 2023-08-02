304b8c9a8440857d866d043a6746210187481473
gp <github@swimr.com>
2023-08-02 14:28:05 -0700

    sqlx

--- push_backend.sh
+++ push_backend.sh
@@ -3,10 +3,9 @@ cargo update;
 make sqlx;
 git add .;
 git commit -am "sqlx"; 
-source ~/.bashrc; git push origin dev; ssh swimr205 "cd dev/rust/_trade/trade;
-export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; 
-git pull origin dev;
-make -B backend"
+source ~/.bashrc;
+git push origin dev;
+ssh swimr205 "cd dev/rust/_trade/trade;export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make -B backend"
 
 # swimr205remote
 #make sqlx; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205remote "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make -B backend"
