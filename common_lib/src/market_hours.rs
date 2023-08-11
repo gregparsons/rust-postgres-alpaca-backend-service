957fc2e9e91921d180b5a39ec731c93e96b874e8
gp <github@swimr.com>
2023-08-11 12:36:46 -0700

    disable after hours causing limit orders and delayed transaction

--- common_lib/src/market_hours.rs
+++ common_lib/src/market_hours.rs
@@ -4,8 +4,9 @@ use chrono::{NaiveTime, Utc};
 use once_cell::sync::Lazy;
 
 // pub const OPERATE_API_AFTER_HOURS: bool = true;
-pub const BUY_EXTENDED_HOURS: bool = true;
-pub const SELL_EXTENDED_HOURS: bool = true;
+// side effect of BUY_EXTENDED_HOURS is causing buy limit orders, which can cause orders to hang, not immediately accepted
+pub const BUY_EXTENDED_HOURS: bool = false;
+pub const SELL_EXTENDED_HOURS: bool = false;
 
 // prod
 pub static MARKET_OPEN_TIME: Lazy<NaiveTime> = Lazy::new(|| NaiveTime::from_hms_opt(9, 30, 0).unwrap()); // 4am Eastern
